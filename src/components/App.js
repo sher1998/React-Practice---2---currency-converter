0b0a3dc123968cb9e1ee27748b728134fce75a5b
Sher Pal Singh <88039223+sher1998@users.noreply.github.com>
2021-08-25 18:46:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,100 +1,15 @@
-import React, { Component, useState, useEffect } from "react";
-
- 
-
-//import "../styles/App.css";
-
- 
+import React  from "react";
+import CurrencyConverter from './CurrencyConverter';
+import '../styles/App.css';
 
 const App = () => {
+  return (
+    <div id="main">
+      <CurrencyConverter/>
+    </div>
+  )
+}
 
- // write your code here
-
- 
-
-const [time, setTime] = useState(0);
-
- const [terminate,setTerminate] = useState(false);
-
- 
-
-useEffect(() => {
-
- !terminate &&
- setTimeout(() => {
-
- time > 0 && setTime(time - 1);
-
- }, 1000);
-
- terminate && setTime(0)
-
- }, [time]);
-
- 
-
-const decreamentCount = (e) => {
-
- if (e.key === "Enter") {
-
-
- console.log(e.target.value)
-
- if (isNaN(e.target.value) || e.target.value == 0) {
-
- setTerminate(true);
-
- } else {
-
- setTerminate(false)
-
- setTime(Math.floor(e.target.value));
-
- }
-
- }
-
- };
-
-console.log(terminate);
-
- return (
-
- 
-
-<div className="wrapper">
-
- <div id="whole-center">
-
- <h1>
-
- Reverse countdown for
-
- <input
-
- id="timeCount"
-
- onKeyDown={decreamentCount /* callback function */}
-
- />{" "}
-
- sec.
-
- </h1>
-
- </div>
-
- 
-
-<div id="current-time">{terminate ?0:time}</div> 
-
-
- </div>
-
- );
-
-};
-
- 
 
 export default App;
+
